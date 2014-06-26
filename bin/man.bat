fc4f5875372f5e6d9ac6cc43a669e8262942088a
Shadab Zafar <dufferzafar0@gmail.com>
2014-06-27 03:48:38 +0530

    Added manpages and a script to view them.
    http://www.mingw.org/wiki/ManScript
    
    Copied manpages from babun's cygwin folder and then extracted all the gz
    files using ExtractNow. I'll now package and distribute these pages so
    others don't have to do it themselves.

---
+++ bin/man.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal
+
+set MSECT=1
+set MPATH=F:\Powerpack\Cmder\manpages
+
+if "%1" == "" (
+  echo Usage: man [-M path] [section] name
+  goto end
+)
+
+if "%1" == "-M" (
+  set MPATH=%2
+  shift
+  shift
+)
+
+if not "%2" == "" (
+  set MSECT=%1
+  shift
+)
+
+:: set MFILE=%MPATH%/man%MSECT%/%1.%MSECT%
+set MFILE=%MPATH%\man%MSECT%\%1.%MSECT%
+
+if not exist %MFILE% (
+  echo Man entry not found
+  echo %MFILE%
+  goto end
+)
+
+groff -Tascii -pet -mandoc -P-c %MFILE% | less -irs
+
+:end
+endlocal
