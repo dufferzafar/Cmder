3cc37e8b44b7b181a6f8799a17fd2c907fbfcb1c
Shadab Zafar <dufferzafar0@gmail.com>
2014-06-26 16:14:49 +0530

    Added cheat, its environment settings and a jrnl cheat.
    Am just going to import a whole lot of cheats from tldr and bropages.

--- vendor/init.bat
+++ vendor/init.bat
@@ -29,10 +29,16 @@
 @set PATH=%PATH%;%git_install_root%\bin;%git_install_root%\cmd;
 
 :: Add Binary Utilities
-@set PATH=%PATH%;%rootDir%\bin;%rootDir%\vendor\file\bin;%rootDir%\vendor\utils;%rootDir%\vendor\gow\bin;%rootDir%\vendor\node-webkit;%rootDir%\vendor\sox;%rootDir%\vendor\imagemagick;
+@set PATH=%PATH%;%rootDir%\bin;%rootDir%\vendor\utils;%rootDir%\vendor\node-webkit;
 
-:: Add 3rd Party Stuff
-@set PATH=%PATH%;%rootDir%\vendor\putty;%rootDir%\vendor\vim\vim74;%rootDir%\vendor\teracopy;
+:: GNU Utils
+@set PATH=%PATH%;F:\Powerpack\Cmder\vendor\groff\bin;%rootDir%\vendor\file\bin;%rootDir%\vendor\gow\bin;
+
+:: Add Moar 3rd Party Stuff
+@set PATH=%PATH%;%rootDir%\vendor\imagemagick;%rootDir%\vendor\putty;%rootDir%\vendor\vim\vim74;%rootDir%\vendor\teracopy;
+
+:: Python 3.3
+@set PATH=%PATH%;C:\Python33\Scripts;
 
 :: Add aliases
 :: These little thingies are life savers, believe me.
@@ -40,6 +46,11 @@
 
 :: Set home path
 @set HOME=%USERPROFILE%
+
+:: Environment settings required by 'cheat'
+@set EDITOR=F:\PowerPack\Cmder\vendor\vim\vim74\vim.exe
+@set DEFAULT_CHEAT_DIR=F:\Powerpack\Cmder\cheats
+@set CHEATCOLORS=true
 
 :: Feel free to modify/remove
 @echo [3;1mWelcome to cmder! customized by [31;mdufferzafar
