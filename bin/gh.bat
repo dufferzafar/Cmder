69dfaa773aa6a3ec7b4758846970cb20d9ecf99c
Shadab Zafar <dufferzafar0@gmail.com>
2014-06-30 21:58:51 +0530

    Removed the gh alias and added a gh.bat instead
    The script shows the git manpage for a command and fallbacks to the
    `git help` command when it can't find a page.
    
    It also has a -d (default) switch which runs `git help` (for times when
    I just prefer the browser than the terminal.)

--- bin/gh.bat
+++ bin/gh.bat
@@ -5,14 +5,13 @@ set MSECT=1
 set MPATH=F:\Powerpack\Cmder\manpages
 
 if "%1" == "" (
-  echo Usage: man [-M path] [section] name
+  echo Usage: gh [section] name
   goto end
 )
 
-if "%1" == "-M" (
-  set MPATH=%2
+if "%1" == "-d" (
   shift
-  shift
+  goto git
 )
 
 if not "%2" == "" (
@@ -20,15 +19,20 @@ if not "%2" == "" (
   shift
 )
 
-set MFILE=%MPATH%\man%MSECT%\%1.%MSECT%
+set MFILE=%MPATH%\man%MSECT%\git-%1.%MSECT%
 
 if not exist %MFILE% (
-  echo Man entry not found
-  echo %MFILE%
-  goto end
+  echo No git man page found for %1 - %MFILE%
+  echo Now starting git help %1 ...
+  echo.
+  goto git
 )
 
 groff -Tascii -pet -mandoc -P-c %MFILE% | less -irs
+goto end
+
+:git
+git help %1
 
 :end
 endlocal
