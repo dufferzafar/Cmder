d35ee4654d58108a0c9c41548153af405ca37767
Shadab Zafar <dufferzafar0@gmail.com>
2014-11-28 21:44:41 +0530

    There seemed to be a bug in a prompt time

--- config/prompt.lua
+++ config/prompt.lua
@@ -1,5 +1,6 @@
 function lambda_prompt_filter()
-    clink.prompt.value = string.gsub(clink.prompt.value, "{time}", string.gsub(os.date("%I:%M"), "^0", ""))
+    time = string.gsub(os.date("%I:%M"), "^0", "")
+    clink.prompt.value = string.gsub(clink.prompt.value, "{time}", time)
 end
 
 clink.prompt.register_filter(lambda_prompt_filter, 40)
